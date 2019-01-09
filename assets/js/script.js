6b7a5d9b2e2b0bc19c2f704fc0887cb441ce7c4e
fkakatie <sextonskatie@gmail.com>
2019-01-08 21:56:28 -0700

    sticky nav and footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,31 @@ $(document).ready(function() {
 
     console.log('javascript file linked.');
 
+    var topnav = $('.navbar');
+    var navCoords = topnav.offset();
+
+    function stickyNav() {
+
+        console.log('stickyNav running');
+
+        console.log(navCoords.top);
+        console.log(window.scrollY);
+
+        if (window.scrollY >= navCoords.top) {
+
+            $('#wrapper').css('padding-top', '48px');
+
+            $('.navbar').addClass('stickyNav');
+
+        } else {
+
+            $('#wrapper').css('padding-top', '0px');
+
+            $('.navbar').removeClass('stickyNav');
+
+        }
+      }
+
+      $(window).on('scroll', stickyNav);
+
 });
