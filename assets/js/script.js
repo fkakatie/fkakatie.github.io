aeb0f38012ac2eb3ba8b82fcef67e0c8d072851f
fkakatie <sextonskatie@gmail.com>
2019-04-14 15:54:47 -0600

    portfolio ui tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,55 +28,15 @@ $(document).ready(function () {
   // check if the sticky nav is needed on every scroll
   $(window).on('scroll', stickyNav);
 
-  // setup owl carousel
-  // $('.owl-carousel').owlCarousel({
-  //     center: true,
-  //     loop: true,
-  //     margin: 15,
-  //     // stagePadding: 50,
-  //     nav: true,
-  //     navText: [
-  //         "<i class='fas fa-angle-left'></i>",
-  //         "<i class='fas fa-angle-right'></i>"
-  //       ],
-  //     autoplay: true,
-  //     autoplayHoverPause: true,
-  //     responsive: {
-  //         0: {
-  //           items: 1
-  //         },
-  //         768: {
-  //           items: 2
-  //         },
-  //         1200: {
-  //           items: 3
-  //         }
-  //       }
-  // });
-
-  // grab carousel item width
-  // var carouselWidth = $('.carouselItem').width() + 30;
-
-  // // set carousel item height to carousel item width
-  // $('.carouselItem').css('height', carouselWidth);
-
-  // function resetCarouselHeight() {
-  //     carouselWidth = $('.carouselItem').width() + 30;
-  //     $('.carouselItem').css('height', carouselWidth);
-  // }
-
-  // reset carousel item height on resize
-  // $(window).on('resize', resetCarouselHeight);
-
   // copyright year in footer
   var year = moment().format("YY");
   var copyYear = '-' + year;
   $('#copyYear').text(copyYear);
 
-  $('#number').text(416);
+  // $('#number').text(416);
 
-  $.get('http://numbersapi.com/416/trivia?notfound=floor&fragment', function (data) {
-    $('#numberData').text(data);
-  });
+  // $.get('http://numbersapi.com/416/trivia?notfound=floor&fragment', function (data) {
+  //   $('#numberData').text(data);
+  // });
 
 });
